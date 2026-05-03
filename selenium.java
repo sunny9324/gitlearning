f11d82dfcbfe97428593335188be9c345f642e7c
sunny9324 <46618459+sunny9324@users.noreply.github.com>
2026-05-03 20:11:59 +1000

    changed url to au via feature 2

--- selenium.java
+++ selenium.java
@@ -2,8 +2,8 @@
 WebDriver driver = new ChromeDriver(); //main
 
 // 2. Navigation: Drive to the store
-driver.get("https://www.google.com");
-
+//driver.get("https://www.google.com");
+driver.get("https://www.google.com.au");//changed url to au via feature 2
 
 // 3. Verification: Check if we arrived at the right place
 String title = driver.getTitle();
