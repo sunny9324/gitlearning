5135bf1c2fd5c389f892b4afddc65331ff41070a
sunny9324 <46618459+sunny9324@users.noreply.github.com>
2026-05-03 19:56:23 +1000

    first selenium script

---
+++ selenium.java
@@ -0,0 +1,13 @@
+// 1. Setup: Get the keys to the car
+WebDriver driver = new ChromeDriver(); //main
+
+// 2. Navigation: Drive to the store
+driver.get("https://www.google.com");
+
+
+// 3. Verification: Check if we arrived at the right place
+String title = driver.getTitle();
+System.out.println("Page title is: " + title);
+
+// 4. Cleanup: Park the car and turn off the engine
+driver.quit();
